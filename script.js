cf56bdfa8a37bef9348dbcf168a81218b95b866e
Filipe de Oliveira de Ataíde <filipe.ataide@lais.huol.ufrn.br>
2024-05-21 11:29:22 -0300

    modais de certo e errado - layout

--- script.js
+++ script.js
@@ -9,6 +9,17 @@ var rows = [
   "Efeito",
 ];
 
+var respotas = [
+  "São os problemas, as dificuldades ou as demandas do conjunto da sociedade ou de alguns grupos específicos reconhecidos como destinatários legítimos de bens e serviços públicos, que a intervenção pública procura resolver ou superar.",
+  "São as enunciações iniciais dos efeitos que se pretendem obter com a intervenção.",
+  "São os bens e serviços empregados na produção de outros bens e serviços que serão entregues a outras organizações ou diretamente ao público alvo de uma intervenção.",
+  "São procedimentos utilizados para converter recursos em produtos. O conceito abrange igualmente a comunicação de informações de gestão e a respectiva utilização pelos gestores.",
+  "São bens ou serviços obtidos por meio da conversão de insumos e que serão entregues como realização da intervenção pública.",
+  "São alterações ou efeitos imediatos experimentados pelos destinatários diretos da intervenção pública.",
+  "São consequências socioeconômicas de longo prazo que podem ser observadas após um determinado período posterior à participação na intervenção e que podem afetar os destinatários diretos da intervenção ou os destinatários indiretos não inseridos na intervenção, que tenham sido por ela beneficiados ou prejudicados.",
+  "É a alteração que resulta da execução da intervenção e que está normalmente relacionada com os seus objetivos. Os efeitos incluem os resultados e os impactos. Podem ser esperados ou não, positivos ou negativos (por exemplo, uma nova estrada que atrai investidores para uma região, mas que causa níveis de poluição inaceitáveis nas zonas que atravessa).",
+];
+
 function printRows() {
   let tr = [];
 
@@ -18,7 +29,7 @@ function printRows() {
             <tr>
                 <th scope="row">${v}</th>
                 <td>
-                    <textarea name="" id="" class="form-control"></textarea>
+                    <textarea name="" id="" class="form-control" data-index="${i}"></textarea>
                 </td>
             </tr>
             `
@@ -29,3 +40,18 @@ function printRows() {
 }
 
 printRows();
+
+/*
+
+$(document).on("change", "textarea", function () {
+  const index = $(this).data("index");
+  console.log($(this).val() == respotas[index]);
+  if ($(this).val() == respotas[index]) {
+    $(this).removeClass("wrong");
+    $(this).addClass("right");
+  } else {
+    $(this).removeClass("right");
+    $(this).addClass("wrong");
+  }
+});
+*/
